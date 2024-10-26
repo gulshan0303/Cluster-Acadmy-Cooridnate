e45d0eb536867dca7918ff6dc73dacafb2dae86e
gkkathare <132427906+gkkathare@users.noreply.github.com>
2024-10-26 15:25:25 +0530

    mongodb error

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
-const connectDb = async(req,res) => {
-     try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("database connection successfully!!")
-     } catch (error) {
-        console.log("database connection failed!!")
-     }
-}
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database connection successfully!!");
+    } catch (error) {
+        console.error("Database connection failed!!", error.message);
+        process.exit(1);
+    }
+};
 
 module.exports = connectDb;
